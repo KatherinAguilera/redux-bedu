f48be984a418c1149b803b582a94796d489c4d05
Katherine Aguilera <katherinam35@gmail.com>
2019-07-04 17:32:06 -0500

    Compartir Reducer

--- src/components/Publicaciones/index.js
+++ src/components/Publicaciones/index.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
- class index extends Component {
+import * as usuariosActions from '../../actions/usuariosActions';
+
+class Publicaciones extends Component {
+	componentDidMount() {
+		// si no hay mas de 0 de usuarios traerlos
+		if (!this.props.usuarios.length) {
+			this.props.traerTodos();
+		}
+	}
 	render() {
+		console.log(this.props);
 		return (
 			<div>
+				<h1>
+					Publicaciones de 
+				</h1>
 				{ this.props.match.params.key }
 			</div>
 		);
 	}
 }
+const mapStateToProps = (reducers) => {
+	return reducers.usuariosReducer;
+};
 
-export default index;
+export default connect(mapStateToProps, usuariosActions)(Publicaciones);
