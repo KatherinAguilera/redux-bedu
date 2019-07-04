7709794ac4adfc743d392d01b914d590b169d6ab
Katherine Aguilera <katherinam35@gmail.com>
2019-07-04 12:34:58 -0500

    Component Spinner

--- src/components/usuarios/index.js
+++ src/components/usuarios/index.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
 import { connect } from 'react-redux';
+import Spinner from '../General/Spinner';
+
 import * as usuariosActions from '../../actions/usuariosActions';
 
 
 class Usuarios extends Component {
 
- componentDidMount() {
+componentDidMount() {
 		// const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users');
 		// this.setState({
 		// 	usuarios: respuesta.data
 		// });
 		this.props.traerTodos();
 	}
+
+	ponerContenido = () => {
+		if (this.props.cargando) {
+			return <Spinner />;
+		}
+
+		return (
+			<table className="tabla">
+				<thead>
+					<tr>
+						<th>
+							Nombre
+						</th>
+						<th>
+							Correo
+						</th>
+						<th>
+							Enlace
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					{ this.ponerFilas() }
+				</tbody>
+			</table>
+		)
+	};
 
 ponerFilas = () => this.props.usuarios.map((usuario) => (
 		<tr key={ usuario.id }>
@@ -28,10 +57,11 @@ ponerFilas = () => this.props.usuarios.map((usuario) => (
 		</tr>
 	));
 
- 	render() {
+	render() {
 		console.log(this.props);
 		return (
 			<div>
+			{ this.ponerContenido() }
 				<table className="tabla">
 					<thead>
 						<tr>
--- src/reducers/usuariosReducer.js
+++ src/reducers/usuariosReducer.js
@@ -1,6 +1,9 @@
-import { TRAER_TODOS } from '../types/usuariosTypes';
+import { TRAER_TODOS, CARGANDO, ERROR } from '../types/usuariosTypes';
+
 const INITIAL_STATE = {
-	usuarios: []
+	usuarios: [],
+	cargando: false,
+	error: ''
 };
 //aquí se crea la función, el estado inicial es un objeto vacío, y la acción es la //"tarea a realizar"
 export default (state = INITIAL_STATE, action) => {
@@ -9,7 +12,17 @@ export default (state = INITIAL_STATE, action) => {
     //la tarea que llegara en esta ocasión es 'traer_usuarios'
 		case TRAER_TODOS:
       //esta parte es destructurar el estado que es un objeto curso de fundamentos //de JavaScripts muy bueno
-			return { ...state, usuarios: action.payload };
+			return {
+				...state,
+				usuarios: action.payload,
+				cargando: false,
+				error: ''
+			};
+		case CARGANDO:
+			return { ...state, cargando: true };
+
+		case ERROR:
+			return { ...state, error: action.payload, cargando: false };
 //despues de todo eso de exporta el reducer y ya esta
 
     default: return state;
